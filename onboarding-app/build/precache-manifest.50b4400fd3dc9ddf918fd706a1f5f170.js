5ebd7511e996b0cb152afad87f039405224d7e38
OlatunjiYso <olatunji.yusuf@audaciousfutures.co>
2020-03-07 01:09:48 +0100

    prepare final deployment

---
+++ onboarding-app/build/precache-manifest.50b4400fd3dc9ddf918fd706a1f5f170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d55e3b69045c88f846ccaca529a55d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fea83c9943f3ae7a2d62",
+    "url": "/static/css/main.74d73cac.chunk.css"
+  },
+  {
+    "revision": "f46e71b7d4c1904c5e86",
+    "url": "/static/js/2.5e44682b.chunk.js"
+  },
+  {
+    "revision": "49c45e15b22906ea61e3b3085d227f58",
+    "url": "/static/js/2.5e44682b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fea83c9943f3ae7a2d62",
+    "url": "/static/js/main.d9d9d50d.chunk.js"
+  },
+  {
+    "revision": "caafc0130372f9fc508f",
+    "url": "/static/js/runtime-main.bbbad617.js"
+  }
+]);
